776f808a4bc8882bc668aa1fb333a39ea80cd412
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2019-05-01 19:35:15 -0500

    tabla

---
+++ Agenda.js
@@ -0,0 +1,48 @@
+import Taller from "./Taller.js";
+
+export default class Agenda {
+    constructor(tablaAgendaTaller) {
+        this._tablaAgendaTaller = tablaAgendaTaller;
+        this._talleres = [];
+        this._initTables();
+    }
+
+    _initTables() {
+        let talleres = JSON.parse(localStorage.getItem("talleres"));
+        if(talleres === null) {
+            return;
+        }
+        talleres.forEach((taller, index) => {
+            taller.fechaInicio = new Date(taller.fechaInicio);
+            taller.fechaTermino = new Date(taller.fechaTermino);
+            this._showInTable(new taller(taller));
+        });
+    }
+    
+    _showInTable(taller) {
+        let row = this._tablaAgendaTaller.insertRow(-1);
+
+        let cellNomTaller = row.insertCell(0);
+        let cellFechaInicio = row.insertCell(1);
+        let cellFechaTermino = row.insertCell(2);
+        let cellLugaresDis = row.insertCell(3);
+        let cellDuracion = row.insertCell(4);
+
+        cellNomTaller.innerHTML = taller.cellNomTaller;
+        cellFechaInicio.innerHTML = taller.getFechaInicio();
+        cellFechaTermino.innerHTML = taller.getFechaTermino();
+        cellLugaresDis.innerHTML = taller.lugaresDis;
+        cellDuracion.innerHTML = taller.duracion;
+
+        let objTaller = {
+            nomTaller: taller.nomTaller,
+            fechaInicio: taller.fechaInicio,
+            fechaTermino: taller.fechaTermino,
+            lugaresDis: taller.lugaresDis,
+            duracion: taller.duracion
+        }
+        this._talleres.push(objTaller);
+
+
+    }
+}
