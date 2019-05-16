9d167e0c0ebea065b0972720381da5867dbc32ca
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2019-05-16 09:57:27 -0500

    mostrar

--- js/infoTalleres.js
+++ js/infoTalleres.js
@@ -15,11 +15,41 @@ $(document).ready(function () {
         opciones += "<option>" + taller.nomTaller + "</option>";
 
         // console.log(taller.nomTaller);
-
-
     });
     document.getElementById("listaTalleres").innerHTML = opciones;
 
+});
+
+//seleccionar el taller
+$('select#listaTalleres').on('change', function () {
+    var valor = $(this).val();
+    document.getElementById("seleccionados").innerHTML = valor;
+});
+
+$('.buttonVer').on('click', function () {
+    //console.log("se dio click");
+    let participantes = JSON.parse(localStorage.getItem("participantes"));
+    let talleres = JSON.parse(localStorage.getItem("talleres"));
+
+    let tallerAsignado = $("#listaTalleres option:selected").text();
+    var nombres = "";
+
+    participantes.forEach((p) => {
+        if (tallerAsignado === p.tallerAsignado) {
+            nombres+= "<label>" + p.nomParticipante+ "</label><br>" ;
+            var lugares = "";
+            talleres.forEach((t,index) => {
+                if (tallerAsignado === t.nomTaller) {
+                    lugares += t.lugaresDis;
+                    document.getElementById("mostrar").innerHTML = "Nombre de taller: " + p.tallerAsignado + " con " + lugares + " lugares disponibles.";
+                   
+                    document.getElementById("mostrar3").innerHTML = "Participantes: "  + nombres;
+                }
+            });
+           
+        }
+    });
+    
 });
 
 
