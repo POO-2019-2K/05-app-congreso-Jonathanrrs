675b626c8ba7338394fc3beedfdaee38a0e21642
Jonathanrrs12 <yoni_12rrs@hotmail.com>
2019-05-16 17:59:18 -0500

    nuevo

--- js/infoTalleres.js
+++ js/infoTalleres.js
@@ -5,19 +5,16 @@ $(document).ready(function () {
 
     if (talleres === null) {
         console.log("no hay talleres")
-        document.getElementById("listaTalleres").innerHTML = noTalleres;
+        document.getElementById("nohayt").innerHTML = "no hay talleres";
         return;
     }
-    var opciones = ""
-
-    talleres.forEach((taller) => {
-
-        opciones += "<option>" + taller.nomTaller + "</option>";
-
-        // console.log(taller.nomTaller);
-    });
-    document.getElementById("listaTalleres").innerHTML = opciones;
-
+    else{
+        var opciones = ""
+        talleres.forEach((taller) => {
+            opciones += "<option>" + taller.nomTaller + "</option>";
+        });
+        document.getElementById("listaTalleres").innerHTML = opciones;
+    }
 });
 
 //seleccionar el taller
@@ -30,26 +27,36 @@ $('.buttonVer').on('click', function () {
     //console.log("se dio click");
     let participantes = JSON.parse(localStorage.getItem("participantes"));
     let talleres = JSON.parse(localStorage.getItem("talleres"));
-
     let tallerAsignado = $("#listaTalleres option:selected").text();
-    var nombres = "";
 
+    if(talleres=== null){
+        
+        document.getElementById("seleccionados").innerHTML = "No hay talleres ";
+    }
+    else{
+
+        if(participantes === null){
+
+        document.getElementById("seleccionados").innerHTML = "No hay participantes ";
+        }
+        else{
+        
+    var nombres = "";
     participantes.forEach((p) => {
         if (tallerAsignado === p.tallerAsignado) {
-            nombres+= "<label>" + p.nomParticipante+ "</label><br>" ;
+            nombres+= "<label>" + p.nomParticipante+ "</label><br>";
             var lugares = "";
-            talleres.forEach((t,index) => {
+            talleres.forEach((t) => {
                 if (tallerAsignado === t.nomTaller) {
                     lugares += t.lugaresDis;
                     document.getElementById("mostrar").innerHTML = "Nombre de taller: " + p.tallerAsignado + " con " + lugares + " lugares disponibles.";
-                   
-                    document.getElementById("mostrar3").innerHTML = "Participantes: "  + nombres;
+                    console.log(p.nomParticipante);
+                    document.getElementById("mostrar3").innerHTML = nombres;
                 }
             });
-           
         }
     });
-    
+    }
+    }
 });
-
 
